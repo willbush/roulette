c4653dafcd3c4dda6601db4a5fd2df7664aad4e8
willbush <will.g.bush@gmail.com>
2019-06-08 00:18:59 -0500

    update build process
    Making this more inline with what I learned from doing another project.

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  pinnedPkgs = import ./pkgs-from-json.nix { json = ./nixos-19-03.json; };
-  myPackages = (import ./release.nix { withHoogle = true; } );
+  pinnedPkgs = import ./nix/pkgs-from-json.nix { json = ./nix/nixos-19-03.json; };
+  myPackages = (import ./nix/release.nix { withHoogle = true; } );
 
   projectDrvEnv = myPackages.project1.env.overrideAttrs (oldAttrs: rec {
     buildInputs = oldAttrs.buildInputs ++ [ pinnedPkgs.haskellPackages.cabal-install ];
